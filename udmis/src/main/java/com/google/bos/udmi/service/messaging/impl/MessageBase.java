c2e99b8c25ba7cadf7599324b761f73b443a6278
Khyati Mahendru <heykhyati@google.com>
2026-03-10 05:57:44 +0000

    undo change default_capacity

--- udmis/src/main/java/com/google/bos/udmi/service/messaging/impl/MessageBase.java
+++ udmis/src/main/java/com/google/bos/udmi/service/messaging/impl/MessageBase.java
@@ -73,7 +73,7 @@ public abstract class MessageBase extends ContainerBase implements MessagePipe {
   private static final Set<Object> HANDLED_QUEUES = new HashSet<>();
   private static final long DEFAULT_POLL_TIME_SEC = 1;
   private static final long AWAIT_TERMINATION_SEC = 10;
-  private static final int DEFAULT_CAPACITY = 100;
+  private static final int DEFAULT_CAPACITY = 1000;
   protected final int queueCapacity;
   protected final long publishDelaySec;
   private final ExecutorService executor = Executors.newFixedThreadPool(EXECUTION_THREADS);
