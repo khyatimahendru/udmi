9fd098345d140c5c13f01fbe5ddfabdddffda3b9
Khyati Mahendru <heykhyati@google.com>
2026-05-08 12:29:09 +0000

    Warn for missing handler instead of always throwing an exception

--- pubber/src/main/java/udmi/lib/base/MqttPublisher.java
+++ pubber/src/main/java/udmi/lib/base/MqttPublisher.java
@@ -765,8 +765,12 @@ public class MqttPublisher implements Publisher {
       Consumer<Object> handler = handlers.get(handlerKey);
       Class<Object> type = handlersType.get(handlerKey);
       if (handler == null) {
-        error(format("Missing handler %s", handlerKey), deviceId, messageType, "receive",
-            new RuntimeException(format("No registered handler for topic %s", topic)));
+        if (this.deviceId.equals(deviceId) && !MqttDevice.ERRORS_TOPIC.equals(messageType)) {
+          error(format("Missing handler %s", handlerKey), deviceId, messageType, "receive",
+              new RuntimeException(format("No registered handler for topic %s", topic)));
+        } else {
+          warn(format("Missing handler %s for topic %s", handlerKey, topic));
+        }
         handlersType.put(handlerKey, Object.class);
         handlers.put(handlerKey, this::ignoringHandler);
         return;
